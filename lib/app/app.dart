63fe2aeac9489d879da90b594dc1ffb1623c9da6
Eshetu Lucas <tekalukas@gmail.com>
2023-11-16 16:32:17 +0300

    Adds dependency injection and route managment

---
+++ lib/app/app.dart
@@ -0,0 +1,25 @@
+import 'package:ejara_test/ui/bottom_sheets/notice/notice_sheet.dart';
+import 'package:ejara_test/ui/views/home/home_view.dart';
+import 'package:ejara_test/ui/views/startup/startup_view.dart';
+import 'package:stacked/stacked_annotations.dart';
+import 'package:stacked_services/stacked_services.dart';
+// @stacked-import
+
+@StackedApp(
+  routes: [
+    MaterialRoute(page: HomeView),
+    MaterialRoute(page: StartupView),
+    // @stacked-route
+  ],
+  dependencies: [
+    LazySingleton(classType: BottomSheetService),
+    LazySingleton(classType: DialogService),
+    LazySingleton(classType: NavigationService),
+    // @stacked-service
+  ],
+  bottomsheets: [
+    StackedBottomsheet(classType: NoticeSheet),
+    // @stacked-bottom-sheet
+  ],
+)
+class App {}
