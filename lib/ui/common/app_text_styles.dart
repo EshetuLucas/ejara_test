c8b6d7be21a5817fab848403c9a9f03b6c561bc2
Eshetu Lucas <tekalukas@gmail.com>
2023-11-16 17:33:41 +0300

    Adds app text styles

---
+++ lib/ui/common/app_text_styles.dart
@@ -0,0 +1,38 @@
+import 'package:ejara_test/ui/common/app_colors.dart';
+import 'package:flutter/material.dart';
+
+const TextStyle ktsSmall = TextStyle(
+  fontWeight: FontWeight.w400,
+  color: kcDarkGreyColor,
+  fontSize: 12,
+);
+
+const TextStyle ktsSmallBold = TextStyle(
+  fontWeight: FontWeight.w600,
+  color: kcDarkGreyColor,
+  fontSize: 12,
+);
+
+const TextStyle ktsMedium = TextStyle(
+  fontWeight: FontWeight.w500,
+  color: kcDarkGreyColor,
+  fontSize: 14,
+);
+
+const TextStyle ktsLargMedium = TextStyle(
+  fontWeight: FontWeight.w400,
+  color: kcDarkGreyColor,
+  fontSize: 16,
+);
+
+const TextStyle ktsLargBold = TextStyle(
+  fontWeight: FontWeight.w600,
+  color: kcDarkGreyColor,
+  fontSize: 24,
+);
+
+const TextStyle ktsTitle = TextStyle(
+  fontWeight: FontWeight.w600,
+  color: kcPrimaryColor,
+  fontSize: 14.5,
+);
