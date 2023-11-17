4492bbe84d746a58bea89e8ca40743ff685e7105
Eshetu Lucas <tekalukas@gmail.com>
2023-11-17 13:59:53 +0300

    Adds user login api

--- lib/api/user/post_apis.dart
+++ lib/api/user/post_apis.dart
@@ -3,18 +3,19 @@ import 'package:ejara_test/api/common/api_consts.dart';
 import 'package:ejara_test/app/app.logger.dart';
 import 'package:ejara_test/data_model/user/user_model.dart';
 
-String getUserUrl = baseUrlV1 + '/accounts/auth/login';
+String loginUrl = baseUrlV1 + '/auth/login';
 
-mixin GetApis {
-  final log = getLogger('UserGetApis');
+mixin PostApis {
+  final log = getLogger('UserPostApis');
   ApiClient get apiClient => ApiClient();
 
   Future<UserModel> login({
     required String userName,
     required String password,
   }) async {
-    return await apiClient.get<UserModel>(
-      getUserUrl,
-    );
+    return await apiClient.post<UserModel>(loginUrl, {
+      "log": userName,
+      "password": password,
+    });
   }
 }
--- lib/api/user/user_apis.dart
+++ lib/api/user/user_apis.dart
@@ -1,6 +1,6 @@
 import 'package:ejara_test/api/common/api_consts.dart';
-import 'package:ejara_test/api/user/get_apis.dart';
+import 'package:ejara_test/api/user/post_apis.dart';
 
 String allBreedsListUrl = baseUrlV1 + '/auth/login';
 
-class UserApis with GetApis {}
+class UserApis with PostApis {}
