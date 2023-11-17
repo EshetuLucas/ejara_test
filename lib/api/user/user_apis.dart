6a255743f3f7fa3a2e84569bc24b5c87f09450ee
Eshetu Lucas <tekalukas@gmail.com>
2023-11-17 18:15:39 +0300

    Updates user apis

--- lib/api/user/user_apis.dart
+++ lib/api/user/user_apis.dart
@@ -1,6 +1,3 @@
-import 'package:ejara_test/api/common/api_consts.dart';
 import 'package:ejara_test/api/user/post_apis.dart';
-
-String allBreedsListUrl = baseUrlV1 + '/auth/login';
 
 class UserApis with PostApis {}
