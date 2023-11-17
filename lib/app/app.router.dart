d336418a6a7391c61811603559d146fcc6ddd994
Eshetu Lucas <tekalukas@gmail.com>
2023-11-18 00:06:13 +0300

    Updates payment service

--- lib/app/app.router.dart
+++ lib/app/app.router.dart
@@ -5,15 +5,18 @@
 // **************************************************************************
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:ejara_test/data_model/payment_method/payment_method.dart'
+    as _i8;
 import 'package:ejara_test/ui/views/home/home_view.dart' as _i2;
 import 'package:ejara_test/ui/views/login/login_view.dart' as _i4;
 import 'package:ejara_test/ui/views/new_payment_setting/new_payment_setting_view.dart'
     as _i5;
 import 'package:ejara_test/ui/views/startup/startup_view.dart' as _i3;
+import 'package:flutter/cupertino.dart' as _i7;
 import 'package:flutter/material.dart' as _i6;
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart' as _i1;
-import 'package:stacked_services/stacked_services.dart' as _i7;
+import 'package:stacked_services/stacked_services.dart' as _i9;
 
 class Routes {
   static const homeView = '/home-view';
@@ -72,8 +75,10 @@ class StackedRouter extends _i1.RouterBase {
       );
     },
     _i5.NewPaymentSettingView: (data) {
+      final args = data.getArgs<NewPaymentSettingViewArguments>(nullOk: false);
       return _i6.MaterialPageRoute<dynamic>(
-        builder: (context) => const _i5.NewPaymentSettingView(),
+        builder: (context) => _i5.NewPaymentSettingView(
+            key: args.key, paymentMethod: args.paymentMethod),
         settings: data,
       );
     },
@@ -86,7 +91,34 @@ class StackedRouter extends _i1.RouterBase {
   Map<Type, _i1.StackedRouteFactory> get pagesMap => _pagesMap;
 }
 
-extension NavigatorStateExtension on _i7.NavigationService {
+class NewPaymentSettingViewArguments {
+  const NewPaymentSettingViewArguments({
+    this.key,
+    required this.paymentMethod,
+  });
+
+  final _i7.Key? key;
+
+  final _i8.PaymentMethodModel paymentMethod;
+
+  @override
+  String toString() {
+    return '{"key": "$key", "paymentMethod": "$paymentMethod"}';
+  }
+
+  @override
+  bool operator ==(covariant NewPaymentSettingViewArguments other) {
+    if (identical(this, other)) return true;
+    return other.key == key && other.paymentMethod == paymentMethod;
+  }
+
+  @override
+  int get hashCode {
+    return key.hashCode ^ paymentMethod.hashCode;
+  }
+}
+
+extension NavigatorStateExtension on _i9.NavigationService {
   Future<dynamic> navigateToHomeView([
     int? routerId,
     bool preventDuplicates = true,
@@ -129,14 +161,18 @@ extension NavigatorStateExtension on _i7.NavigationService {
         transition: transition);
   }
 
-  Future<dynamic> navigateToNewPaymentSettingView([
+  Future<dynamic> navigateToNewPaymentSettingView({
+    _i7.Key? key,
+    required _i8.PaymentMethodModel paymentMethod,
     int? routerId,
     bool preventDuplicates = true,
     Map<String, String>? parameters,
     Widget Function(BuildContext, Animation<double>, Animation<double>, Widget)?
         transition,
-  ]) async {
+  }) async {
     return navigateTo<dynamic>(Routes.newPaymentSettingView,
+        arguments: NewPaymentSettingViewArguments(
+            key: key, paymentMethod: paymentMethod),
         id: routerId,
         preventDuplicates: preventDuplicates,
         parameters: parameters,
@@ -185,14 +221,18 @@ extension NavigatorStateExtension on _i7.NavigationService {
         transition: transition);
   }
 
-  Future<dynamic> replaceWithNewPaymentSettingView([
+  Future<dynamic> replaceWithNewPaymentSettingView({
+    _i7.Key? key,
+    required _i8.PaymentMethodModel paymentMethod,
     int? routerId,
     bool preventDuplicates = true,
     Map<String, String>? parameters,
     Widget Function(BuildContext, Animation<double>, Animation<double>, Widget)?
         transition,
-  ]) async {
+  }) async {
     return replaceWith<dynamic>(Routes.newPaymentSettingView,
+        arguments: NewPaymentSettingViewArguments(
+            key: key, paymentMethod: paymentMethod),
         id: routerId,
         preventDuplicates: preventDuplicates,
         parameters: parameters,
