b614b4a55a6273f2af4c2306b104b11f761ce17b
Eshetu Lucas <tekalukas@gmail.com>
2023-11-17 18:21:51 +0300

    Removes un used bottom_sheet

---
+++ lib/ui/bottom_sheets/wallets/wallets_sheet_model.dart
@@ -0,0 +1,86 @@
+import 'package:ejara_test/app/app.locator.dart';
+import 'package:ejara_test/app/app.logger.dart';
+import 'package:ejara_test/data_model/payment_method/payment_method.dart';
+import 'package:ejara_test/data_model/payment_setting/payment_setting.dart';
+import 'package:ejara_test/services/payment_service.dart';
+import 'package:flutter/material.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+class WalletsSheetModel extends ChangeNotifier {
+  final log = getLogger('WalletsSheetModel');
+  final _paymentService = locator<PaymentService>();
+  final _navigationService = locator<NavigationService>();
+
+  String _errorText = '';
+  String get errorText => _errorText;
+
+  void setErrorText(String value) {
+    _errorText = value;
+    notifyListeners();
+  }
+
+  void setBusy(bool value) {
+    _isBusy = value;
+    notifyListeners();
+  }
+
+  bool _hasError = false;
+  bool get hasError => _hasError;
+
+  bool _isBusy = false;
+  bool get isBusy => _isBusy;
+
+  PaymentSettingModel? _selectedPaymentSetting;
+  bool get hasSelectedPaymentSetting => _selectedPaymentSetting != null;
+
+  bool isPaymentSettingSelected(int paymentSettingId) =>
+      hasSelectedPaymentSetting &&
+      _selectedPaymentSetting!.id == paymentSettingId;
+
+  bool get hasPaymentSettings => paymentSettings.isNotEmpty;
+
+  void onSelectPaymentSetting(PaymentSettingModel paymentSetting) {
+    _selectedPaymentSetting = paymentSetting;
+    notifyListeners();
+  }
+
+  List<PaymentSettingModel> _paymentSettings = [];
+  List<PaymentSettingModel> get paymentSettings =>
+      isBusy ? fakePaymentSettings : _paymentSettings;
+
+  Future<void> getPaymentSettingsByType(
+      PaymentMethodModel paymentMethod) async {
+    setBusy(true);
+    setError(true);
+    try {
+      /// Using const countryCode and transactionType for testing purposes
+      // _paymentMethods =
+      await _paymentService.getPaymentSettingsByType(
+        countryCode: 'CM',
+        transactionType: 'buy',
+        paymentTypeId: paymentMethod.id.toString(),
+      );
+    } catch (e) {
+      setError(true);
+      log.e('Unable to get payment methods');
+    } finally {
+      setBusy(false);
+    }
+  }
+
+  void setError(bool value) {
+    _hasError = value;
+    notifyListeners();
+  }
+
+  void onClose() {
+    _navigationService.back();
+  }
+
+  void disposeViewModel() {
+    _selectedPaymentSetting = null;
+    _paymentSettings = [];
+  }
+
+  void onConfirm() {}
+}
