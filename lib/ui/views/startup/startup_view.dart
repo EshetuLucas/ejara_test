31824df2ad3dca9ef68e62ece17b0d15a4f29904
Eshetu Lucas <tekalukas@gmail.com>
2023-11-17 18:18:50 +0300

    Updates startup view

--- lib/ui/views/startup/startup_view.dart
+++ lib/ui/views/startup/startup_view.dart
@@ -1,41 +1,44 @@
 import 'package:ejara_test/ui/common/app_colors.dart';
+import 'package:ejara_test/ui/common/app_text_styles.dart';
 import 'package:ejara_test/ui/common/widgets/stateful_wrapper.dart';
 import 'package:flutter/material.dart';
 import 'package:ejara_test/ui/common/ui_helpers.dart';
 import 'package:provider/provider.dart';
 
-import 'startup_viewmodel.dart';
+import 'starup_viewmodel.dart';
 
-class StartupView extends StatelessWidget {
-  const StartupView({Key? key}) : super(key: key);
+class StarupView extends StatelessWidget {
+  const StarupView({Key? key}) : super(key: key);
 
   @override
   Widget build(
     BuildContext context,
   ) {
-    final viewModel = Provider.of<StartupViewModel>(context, listen: false);
+    final viewModel = Provider.of<StarupViewModel>(context, listen: true);
     return StatefulWrapper(
-      onInit: viewModel.runStartupLogic,
-      child: const Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  horizontalSpaceSmall,
-                  SizedBox(
-                    width: 17,
-                    height: 17,
-                    child: CircularProgressIndicator(
-                      color: kcPrimaryColor,
-                      strokeWidth: 6,
-                    ),
-                  )
-                ],
-              ),
-            ],
+      onInit: viewModel.runStarupLogic,
+      child: Scaffold(
+        body: Padding(
+          padding: appSymmetricHorizontalPadding,
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                horizontalSpaceSmall,
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      Text(
+                        'Startup view',
+                        style: ktsMedium.copyWith(color: kcRed),
+                      ),
+                      verticalSpaceSmall,
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
---
+++ lib/ui/views/startup/starup_viewmodel.dart
@@ -0,0 +1,9 @@
+import 'package:ejara_test/app/app.logger.dart';
+
+import 'package:flutter/cupertino.dart';
+
+class StarupViewModel extends ChangeNotifier {
+  final log = getLogger('StartupViewModel');
+
+  Future<void> runStarupLogic() async {}
+}
